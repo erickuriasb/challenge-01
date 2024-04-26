638af673453e539188c1336837a13662d41c1230
erickuriasb <erickuriasb@gmail.com>
2024-04-26 11:04:36 -0600

    Validate no name field empty

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ function validate_name(){
     return
 }
 
+const input_name = document.querySelector('#name');
+const input_emain = document.querySelector('#email');
 const btn_step_1 = document.querySelector('#btn_step_1');
 const btn_step_2 = document.querySelector('#btn_step_2');
 const btn_step_3 = document.querySelector('#btn_step_3');
 const topics = document.querySelector('#topics');
 let row_step_1 = document.querySelector('#row-step-1');
+let card_step_1 = document.querySelector('#card-first-step');
 let row_step_2 = document.querySelector('#row-step-2');
 let row_step_3 = document.querySelector('#row-step-3');
 let selected_topics = document.querySelector('#selected-topics');
@@ -16,6 +19,7 @@ let data = {
     email: "",
     topics: [],
 }
+
 
 for( let i=0 ; i < topics.childElementCount; i++){
     let currentTopic = topics.children[i].firstElementChild
@@ -30,12 +34,22 @@ for( let i=0 ; i < topics.childElementCount; i++){
 
 btn_step_1.addEventListener('click', (event) => {
     event.preventDefault();
-    row_step_1.classList.add('d-none');
-    row_step_2.classList.remove('d-none');
-    const name = document.querySelector('#name');
-    const email = document.querySelector('#email');
-    data.name = name.value;
-    data.email = email.value;
+    if (input_name.value === ""){
+        const node = document.createElement('div');
+        const textNode = document.createTextNode('Please Fill out required fields!');
+        node.classList.add('text-danger','mt-2');
+        node.append(textNode);
+        card_step_1.appendChild(node);
+    }else{
+        row_step_1.classList.add('d-none');
+        row_step_2.classList.remove('d-none');
+        const name = document.querySelector('#name');
+        const email = document.querySelector('#email');
+        data.name = name.value;
+        data.email = email.value;
+    }
+
+
 });
 
 btn_step_2.addEventListener('click', (event) => {
