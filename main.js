6c960e4a667e6d95461f70b37bd702595e871ba7
erickuriasb <erickuriasb@gmail.com>
2024-04-26 12:05:28 -0600

    improve validation message for name and email

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function validate_name(){
 }
 
 const input_name = document.querySelector('#name');
-const input_emain = document.querySelector('#email');
+const input_email = document.querySelector('#email');
 const btn_step_1 = document.querySelector('#btn_step_1');
 const btn_step_2 = document.querySelector('#btn_step_2');
 const btn_step_3 = document.querySelector('#btn_step_3');
@@ -34,12 +34,16 @@ for( let i=0 ; i < topics.childElementCount; i++){
 
 btn_step_1.addEventListener('click', (event) => {
     event.preventDefault();
-    if (input_name.value === ""){
-        const node = document.createElement('div');
-        const textNode = document.createTextNode('Please Fill out required fields!');
-        node.classList.add('text-danger','mt-2');
-        node.append(textNode);
-        card_step_1.appendChild(node);
+    if (validate_name() || validate_email()){
+        let message = document.querySelector('#message');
+        if (message.childElementCount == 0){
+            const node = document.createElement('div');
+            const textNode = document.createTextNode('Please Fill out required fields!');
+            node.classList.add('text-danger','mt-2');
+            node.append(textNode);
+            message.appendChild(node);
+            markRequiredFields();
+        }
     }else{
         row_step_1.classList.add('d-none');
         row_step_2.classList.remove('d-none');
@@ -51,6 +55,32 @@ btn_step_1.addEventListener('click', (event) => {
 
 
 });
+
+function validate_name(){
+    if (input_name.value === ""){
+        return true;
+    }else{
+        return false;
+    }
+}
+function validate_email(){
+    if (input_email.value === ""){
+        return true;
+    }else{
+        
+        return false;
+    }
+}
+function markRequiredFields(){
+    let labels = document.querySelectorAll('label');
+    for( let i = 0; i < labels.length; i++){
+        const node = document.createElement('span');
+        node.classList.add('text-danger');
+        const char = document.createTextNode(' *');
+        node.appendChild(char);
+        labels[i].appendChild(node);
+    }
+}
 
 btn_step_2.addEventListener('click', (event) => {
     event.preventDefault();
@@ -74,6 +104,6 @@ btn_step_2.addEventListener('click', (event) => {
 });
 
 btn_step_3.addEventListener('click', (event) => {
-    //event.preventDefault();
+    event.preventDefault();
 });
 
